8e5c4992ac0c08a9895575a2d8465d5b89b74420
sohnemann <gitlab@kioku.de>
2024-08-31 13:27:23 +1200

    added service

--- install-docker.sh
+++ install-docker.sh
@@ -1,2 +1,11 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh --dry-run
+sudo sh ./get-docker.sh
+
+sudo sh -eux <<EOF
+# Install newuidmap & newgidmap binaries
+apt-get install -y uidmap
+EOF
+
+dockerd-rootless-setuptool.sh install
+
+sudo apt-get install docker-compose -y
